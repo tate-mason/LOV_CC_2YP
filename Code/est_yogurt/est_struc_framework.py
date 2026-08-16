abc8a86482a0e15964a1ad7ee89d4b61aa79694c
tate-mason <tatem8499@gmail.com>
2026-08-16 11:55:28 -0400

    la

--- Code/est_yogurt/est_struc_framework.py
+++ Code/est_yogurt/est_struc_framework.py
@@ -110,7 +110,7 @@ trips_df = full_panel[[
     'upc', 'product_module_code', 'trip_code_uc', 'household_code',
     'week_end', 'purchase_date', 'store_code_uc', 'flavor_binary',
     'quantity', 'household_income', 'male_head_age', 'female_head_age',
-    'type_of_residence', 'race'
+    'type_of_residence', 'race', 'price'
 ]]
 
 yog    = full_panel[full_panel['product_module_code'].isin([3612,3603])]

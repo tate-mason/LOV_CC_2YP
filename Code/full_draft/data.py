65b371adec2f2f865f5f0008c2c058fbab714fb4
tate-mason <tatem8499@gmail.com>
2026-08-19 17:56:08 -0400

    la

--- Code/full_draft/data.py
+++ Code/full_draft/data.py
@@ -140,7 +140,7 @@ agent_yogurt = agent_yogurt[agent_yogurt['product_module_code'].isin([3612, 3603
 console.print(
     f'Number of households in full sample:                  {agent_master['household_code'].nunique()}\n',
     f'Mean number of trips per HH:                          {agent_master.groupby('household_code')['trip_code_uc'].nunique().mean()}\n',
-    f'Number of yogurt purchases among purchasers per trip: {agent_yogurt.groupby(['household_code', 'trip_code_uc', 'flavor'])['quantity'].mean()}\n',
+    f'Number of yogurt purchases among purchasers per trip: {agent_yogurt.groupby(['household_code', 'trip_code_uc', 'plain'])['quantity'].mean()}\n',
     f'Mean household income:                                {agent_master['household_income'].mean()}\n',
     f'Median household income:                              {agent_master['household_income'].median()}\n',
     f'Racial makeup of sample:                              {agent_master.groupby('race')['household_code'].nunique()}\n',

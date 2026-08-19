9cf5b47e9490d21784321d4f4a233a8a6dd21436
tate-mason <tatem8499@gmail.com>
2026-08-19 18:00:24 -0400

    la

--- Code/full_draft/data.py
+++ Code/full_draft/data.py
@@ -145,7 +145,7 @@ console.print(
     f'Median household income:                              {agent_master['household_income'].median()}\n',
     f'Racial makeup of sample:                              {agent_master.groupby('race')['household_code'].nunique()}\n',
     f'Percent taking outside option each trip:              {outside_option['outside_option_rate'].mean()}\n',
-    f'Percent purchasing with coupon:                       {agent_master.groupby(['household_code', 'deal_flag_uc'])['yogurt_buy'].mean()}\n',
+    f'Percent purchasing with coupon:                       {agent_master.groupby(['household_code', 'deal_flag_uc'])['yogurt_purchase'].mean()}\n',
 )
 
 #=== Switching Stats ===#

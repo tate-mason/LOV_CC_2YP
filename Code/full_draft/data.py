c08ed75c956969952e0d35ddaeff514d69e341b7
tate-mason <tatem8499@gmail.com>
2026-08-23 15:37:34 -0400

    legend

--- Code/full_draft/data.py
+++ Code/full_draft/data.py
@@ -343,12 +343,20 @@ weekly_dma_price = (
     .mean()
     .reset_index()
 )
-custom_colors = {
+custom_colors = dma_map = {
+    524: 'Atlanta',
+    602: 'Chicago',
+    751: 'Denver',
+    825: 'San Diego'
+}
+
+merged_master['market_name'] = merged_master['dma_code'].map(dma_map){
     'Atlanta': 'firebrick',
     'Chicago': 'navy',
     'Denver': 'forestgreen',
     'San Diego': 'cyan'
 }
+
 fig, ax = plt.subplots(figsize=(9, 4.5))
 sns.lineplot(
     data=weekly_dma_price,
@@ -364,7 +372,7 @@ sns.lineplot(
 ax.set_xlabel('Week', fontsize=11)
 ax.set_ylabel('Mean Unit Price ($)', fontsize=11)
 ax.set_title('Weekly Yogurt Price Variation Across Markets (2014)', fontsize=12, fontweight='bold')
-ax.legend(title='Market (DMA)', frameon=True)
+ax.legend(title='Market', frameon=True)
 ax.grid(True, linestyle='--', alpha=0.5)
 
 plt.tight_layout()

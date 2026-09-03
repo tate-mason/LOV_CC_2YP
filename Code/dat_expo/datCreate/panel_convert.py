4a799f085f321f3f2db672fcc6b63221a1b6088e
Tate Mason <tatem8499@gmail.com>
2026-09-03 14:37:12 -0400

    panel shi

--- Code/dat_expo/datCreate/panel_convert.py
+++ Code/dat_expo/datCreate/panel_convert.py
@@ -1,52 +1,51 @@
 import polars as pl
 
-years = [2017, 2018, 2019]
+years = [2022, 2023, 2024]
 
 for y in years:
     print(f"loading panelists {y}")
     panelists = (
-        pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{y}/Annual_Files/panelists_{y}.tsv', separator='\t')
+        pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{y}/Annual_Files/panelist.tsv', separator='\t')
         .sink_parquet(f'../../nielsen_extracts/panelists_{y}.parquet')
     )
     print(f'Panelists converted for {y}')
 
     print('loading purchases')
     purchases = (
-        pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{y}/Annual_Files/purchases_{y}.tsv', separator='\t')
+        pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{y}/Annual_Files/purchase.tsv', separator='\t')
         .sink_parquet(f'../../nielsen_extracts/purchases_{y}.parquet')
     )
     print('Purchases converted')
 
     print('loading trips')
     trip = (
-        pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{y}/Annual_Files/trips_{y}.tsv', separator='\t')
+        pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{y}/Annual_Files/trip.tsv', separator='\t')
         .sink_parquet(f'../../nielsen_extracts/trips_{y}.parquet')
     )
     print('Trips converted')
-
-print(f'loading products')
-products = (
-    pl.scan_csv(
-        '/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/Master_Files/Latest/products.tsv', 
-        separator='\t', 
-        quote_char=None,
-        encoding='utf8-lossy'
+    
+    print('loading product attributes')
+    prod_att = (
+    		pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{y}/Annual_Files/productattributes.tsv', separator='\t')
+        .sink_parquet(f'../../nielsen_extracts/product_attr_{y}')
     )
-    .sink_parquet('../../nielsen_extracts/products.parquet')
-)
-print('Products converted')
-
-print(f'loading retailers')
-retailers = (
-    pl.scan_csv(
-        '/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/Master_Files/Latest/retailers.tsv',
-        separator='\t',
-        quote_char=None,
-        encoding='utf8-lossy'
+    print('Product Attributes Converted')
+    
+    print('loading product descriptions')
+    prod_desc = (
+		pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{y}/Annual_Files/productdesc.tsv', separator='\t')
+        .sink_parquet(f'../../nielsen_extracts/product_desc_{y}')
     )
-    .sink_parquet('../../nielsen_extracts/retailers.parquet')
-)
-print('Retailers converted')
+    print('descriptions converted')
+    
+    print('loading retailers')
+    retailers = (
+    		pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{y}/Annual_Files/retailer.tsv', separator='\t')
+    )
+    print('retailers converted')
+
+
+
 
 print('-'*60)
 print('All Files Converted from .tsv to .parquet - Move to Merge')

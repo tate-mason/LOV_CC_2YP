d324a8d24938e65b1749000236b42e62fa7f20d1
tate-mason <tatem8499@gmail.com>
2026-09-05 13:37:12 -0400

    wrong syntax

--- Code/dat_expo/code_check.py
+++ Code/dat_expo/code_check.py
@@ -2,7 +2,7 @@ import polars as pl
 
 df = pl.scan_parquet(f'../../nielsen_extracts/HMS/panelists_2022.parquet')
 
-print(df.columns.to_list())
+print(df.columns)
 
 #print(df.select(['dma_code', 'dma_descr']))
 

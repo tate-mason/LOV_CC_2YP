7746b1fbb4caefc60d71edcc512fd5e5eff491f5
tate-mason <tatem8499@gmail.com>
2026-09-07 19:24:29 -0400

    market level data

--- Code/dat_expo/panel_merge.py
+++ Code/dat_expo/panel_merge.py
@@ -3,42 +3,54 @@ import polars as pl
 import scipy as sp
 import gc
 
+output_dir = "/scratch/dtm63837/Kilts_Panel/nielsen_extracts/output_markets"
+os.makedirs(output_dir, exist_ok=True)
 # data sets being loaded
 dat = ['panelists', 'purchases', 'trips', 'product_attr', 'product_desc', 'retailer']
 years = [2022, 2023, 2024]
+valid_codes = {
+    "Atlanta": [f'{x:05d}' for x in range(13010, 13300)],
+    "Chicago": [f'{x:05d}' for x in range(17031, 18128)],
+    "Houston": [f'{x:05d}' for x in range(48015, 48482)],
+    "Denver":  [f'{x:05d}' for x in range(8001, 8126)],
+    "Phoenix": [f'{x:05d}' for x in range(4007, 4026)],
+    "Philadelphia": [
+        f'{x:05d}' for x in set().union(
+            range(34001, 34004),
+            range(42017, 42102)
+        )
+    ],
+    "San Diego": ["06073"],
+    "Des Moines":[f'{x:05d}' for x in range(19001, 19199)]
+}
 
-# loop for loading all parquet
-frame = {}
-for d, y in product(dat, years):
-    print(f'Loading {d}, {y}')
-    lazy_df = pl.scan_parquet(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{d}_{y}.parquet').rename(str.lower)
-    frame[(d,y)] = lazy_df
-    print(f'{d} loaded')
-combined_frame = {}
-for d in dat:
-    yearly_lfs   = [frame[(d,y)] for y in years]
-    combined_lfs = pl.concat(yearly_lfs, how='diagonal_relaxed')
-    combined_frame[d] = combined_lfs
-    globals()[f'{d}'] = combined_lfs 
+market_df = {}
 
-print(panelists.columns)
+for m, codes in valid_codes.items():
+    market_dfs[m] = {}
+
+    for d in dat:
+        yearly_lfs = []
+        for y in years:
+            file_path = f"/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{d}_{y}.parquet"
+
+            lazy_df = (
+                pl.scan_parquet(file_path)
+                .rename(str.lower)
+                .filter(pl.col('fips_county_code').cast(pl.Utf8).is_in(codes))
+            )
+            yearly_lfs.append(lazy_df)
+        combined_lazy = pl.concat(yearly_lfs, how='diagonal_relaxed')
+        df_dataset = combined_lazy.collect()
+        market_dfs[m][d] = df_dataset 
+
+        out_file = os.path.join(output_dir, f'{m}_{d}.parquet')
+        df_dataset.write_parquet(out_file)
+        print(f'--> Saved {out_file} ({df_dataset.height:,} rows)')
 
 # merging trips and panelists
 # Build a single set containing all numbers across all ranges
-valid_codes = set().union(
-    range(13010, 13300),
-    range(17031, 18128),
-    range(48015, 48482),
-    range(8001, 8126),
-    range(4007, 4026),
-    range(34001, 34004),
-    range(42017, 42102),
-    [6073],
-    range(19001, 19198)
-)
-str_codes = [f"{x:05d}" for x in valid_codes]
-panelists = panelists.filter(pl.col('fips_county_code').is_in(str_codes))
-trip_panelists = trips.join(panelists, on = ['panel_year', 'household_code'], how='left')
+#trip_panelists = trips.join(panelists, on = ['panel_year', 'household_code'], how='left')
 # Atlanta, Chicago, Denver, Des Moines, San Diego, Philly, Houston, Phoenix 
 # .is_in(13010:13299), (17031:18127), (48015:48481), (08001:08125), (04007:04025), ((34001:34003), (42017:42101)), 06073, (19001:19197)
 del trips, panelists
@@ -46,24 +58,24 @@ gc.collect()
 
 # merge purchases and trip_panelists
 
-tpp = trip_panelists.join(purchases, on = 'trip_code_uc', how='left')
-del trip_panelists, purchases
-gc.collect()
-
-tpp_r = tpp.join(retailer, on = 'retailer_code', how='left')
-del tpp, retailer
-gc.collect()
-
-products = product_attr.join(product_desc, on = 'upc', how = 'left')
-del product_attr, product_desc
-gc.collect()
-
-master = (
-        tpp_r.join(products, on = 'upc')
-        .collect(streaming=False)
-        .write_parquet('/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/master_panel.parquet')
-)
-del tpp_r, products
-gc.collect()
-
-print("done")
+#tpp = trip_panelists.join(purchases, on = 'trip_code_uc', how='left')
+#del trip_panelists, purchases
+#gc.collect()
+#
+#tpp_r = tpp.join(retailer, on = 'retailer_code', how='left')
+#del tpp, retailer
+#gc.collect()
+#
+#products = product_attr.join(product_desc, on = 'upc', how = 'left')
+#del product_attr, product_desc
+#gc.collect()
+#
+#master = (
+#        tpp_r.join(products, on = 'upc')
+#        .collect(streaming=False)
+#        .write_parquet('/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/master_panel.parquet')
+#)
+#del tpp_r, products
+#gc.collect()
+#
+#print("done")

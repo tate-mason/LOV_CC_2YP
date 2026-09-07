a688a64c3d5fe2f96521280152c687718bbacf31
tate-mason <tatem8499@gmail.com>
2026-09-07 16:51:19 -0400

    forgot

--- Code/dat_expo/code_check.py
+++ Code/dat_expo/code_check.py
@@ -3,6 +3,6 @@ pd.set_option('display.max_rows', None)
 
 df = pd.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documetation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx', dtype=str)
 
-mapping_df = df.iloc[:,[4,5]].drop_duplicates().reset_index(drop=True)
+mapping_df = df.iloc[:,[1, 2, 3, 4,5]].drop_duplicates().reset_index(drop=True)
 
 print(mapping_df)

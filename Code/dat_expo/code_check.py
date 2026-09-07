2f0deb0d48840e428c836f50c973ad70e0f70655
tate-mason <tatem8499@gmail.com>
2026-09-07 16:45:17 -0400

    la

--- Code/dat_expo/code_check.py
+++ Code/dat_expo/code_check.py
@@ -4,8 +4,4 @@ df = pd.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-O
 
 mapping_df = df.iloc[:,[4,5]].drop_duplicates().reset_index(drop=True)
 
-for i in range(len(mapping_df)):
-    dma_code  = df.iloc[i, 4]
-    dma_label = df.iloc[i, 5]
-
-    print(f'DMA Code: {dma_code} --> DMA: {dma_label}')
+print(mapping_df)

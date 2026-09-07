f59d019140350d7f2f589bb07168e2d7230d3c14
tate-mason <tatem8499@gmail.com>
2026-09-07 16:43:11 -0400

    better output

--- Code/dat_expo/code_check.py
+++ Code/dat_expo/code_check.py
@@ -2,10 +2,9 @@ import pandas as pd
 
 df = pd.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documetation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx', dtype=str)
 
-df.iloc[:,5] = df.iloc[:,5].str.strip()
-df.iloc[:,4] = df.iloc[:,4].str.strip()
+mapping_df = df.iloc[:,[4,5]].drop_duplicates().reset_index(drop=True)
 
-for i in range(len(df)):
+for i in range(len(mapping_df)):
     dma_code  = df.iloc[i, 4]
     dma_label = df.iloc[i, 5]
 

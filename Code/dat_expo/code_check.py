3561ed924cdcc48ca668d10713c222bf79e49fdc
tate-mason <tatem8499@gmail.com>
2026-09-07 16:21:52 -0400

    tables for markets

--- Code/dat_expo/code_check.py
+++ Code/dat_expo/code_check.py
@@ -1,7 +1,36 @@
 import polars as pl
+pl.Config.set_fmt_str_lengths(1000)  # Max characters per string/cell
+pl.Config.set_tbl_formatting("ASCII_FULL")
+
+# 2. Prevent row/column truncation in the terminal/notebook
+pl.Config.set_tbl_rows(-1)       # Show ALL rows (-1 means unlimited)
+pl.Config.set_tbl_cols(-1)       # Show ALL columns
+pl.Config.set_fmt_table_cell_list_len(-1)  # Show ALL elements in a list column
 
 df = pl.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documetation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx')
 
 print(df.columns)
 
+res_DMA = (
+    df 
+    .with_columns(pl.col('DMA Name').str.strip_chars())
+    .groupby('DMA Name')
+    .agg(
+        pl.col('DMA Code').drop_nulls().unique().alias('Associated_DMA'),
+        pl.col('DMA Code').drop_nulls().n_unique().alias('Unique_DMA')
+    )
+)
 
+res_FIPS = (
+    df
+    .with_columns(pl.col('County Name').strip_chars())
+    .groupby('County Name')
+    .agg(
+        pl.col('FIPS Code').drop_nulls().unique().alias('Associated_FIPS'),
+        pl.col('FIPS Code').drop_nulls().n_unique().alias('Unique_FIPS')
+    )
+)
+
+print(res_DMA)
+print('='*50)
+print(res_FIPS)

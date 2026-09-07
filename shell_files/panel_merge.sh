4e7a392e58eada1df6b98abd0cde2f8abe0915f3
tate-mason <dtm63837@uga.edu>
2026-09-07 16:15:53 -0400

    updated reqs

--- shell_files/panel_merge.sh
+++ shell_files/panel_merge.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1			# Number of compute nodes for resources to be spread out over (increase only if using MPI enabled software)
 #SBATCH --ntasks=1			# 1 task (process) for below commands
 #SBATCH --cpus-per-task=1	 	# CPU core count per task, by default 1 CPU core per task
-#SBATCH --mem=125G			# Memory per node (4GB); by default using M as unit
+#SBATCH --mem=500G			# Memory per node (4GB); by default using M as unit
 #SBATCH --time=38:00:00              	# Time limit hrs:min:sec or days-hours:minutes:seconds
 #SBATCH --output=%x_%j.out		# Standard output log, e.g., testBowtie2_12345.out
 #SBATCH --mail-user=dtm63837@uga.edu    # Where to send mail

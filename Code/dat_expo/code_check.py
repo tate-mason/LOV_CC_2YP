3ea4e6b7abc5378ed7652dc1bfecb0fd20a55e30
tate-mason <tatem8499@gmail.com>
2026-09-07 16:13:51 -0400

    hm

--- Code/dat_expo/code_check.py
+++ Code/dat_expo/code_check.py
@@ -1,9 +1,7 @@
 import polars as pl
 
-df = pl.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documentation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx')
+df = pl.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documetation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx')
 
 print(df.columns)
-
-#print(df.select(['dma_code', 'dma_descr']))
 
 

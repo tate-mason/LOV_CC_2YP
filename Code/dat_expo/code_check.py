4ced8ecb3d0e6679c5165339853215c1d8776f4f
tate-mason <tatem8499@gmail.com>
2026-09-07 16:39:33 -0400

    la

--- Code/dat_expo/code_check.py
+++ Code/dat_expo/code_check.py
@@ -1,59 +1,12 @@
-import polars as pl
-pl.Config.set_fmt_str_lengths(1000)  # Max characters per string/cell
-pl.Config.set_tbl_formatting("ASCII_FULL")
+import pandas as pd
 
-# 2. Prevent row/column truncation in the terminal/notebook
-pl.Config.set_tbl_rows(-1)       # Show ALL rows (-1 means unlimited)
-pl.Config.set_tbl_cols(-1)       # Show ALL columns
-pl.Config.set_fmt_table_cell_list_len(-1)  # Show ALL elements in a list column
+df = pd.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documetation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx', dtype=str)
 
-df = pl.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documetation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx')
+df.iloc[:,5] = df.iloc[:,5].str.strip()
+df.iloc[:,4] = df.iloc[:,4].str.strip()
 
-print(df.columns)
-print(df.schema)
+for i in range(len(df)):
+    dma_code  = df.iloc[i, 4]
+    dma_label = df.iloc[i, 5]
 
-res_DMA = (
-    df 
-    .with_columns(
-        pl.col('DMA Name').str.strip_chars(),
-        pl.col('DMA code').str.strip_chars()
-    )
-    .group_by('DMA Name')
-    .agg(
-        pl.col('DMA code')
-        .filter(pl.col('DMA code').is_not_null() & (pl.col('DMA code') != ""))
-        .unique()
-        .sort()
-        .list.join(", ")
-        .alias('associated_dma'),
-        # unique vals
-        pl.col('DMA code')
-        .n_unique()
-        .alias('Unique_DMA')
-    )
-)
-
-res_FIPS = (
-    df
-    .with_columns(
-        pl.col('County Name').str.strip_chars(),
-        pl.col('FIPS Code').str.strip_chars()
-    )
-    .group_by('County Name')
-    .agg(
-        pl.col('FIPS Code')
-        .filter(pl.col('FIPS Code').is_not_null() & (pl.col('FIPS Code') != ""))
-        .unique()
-        .sort()
-        .list.join(", ")
-        .alias('Associated_FIPS'),
-        # unique vals
-        pl.col('FIPS Code')
-        .n_unique()
-        .alias('Unique_FIPS')
-    )
-)
-
-print(res_DMA)
-print('='*50)
-print(res_FIPS)
+    print(f'DMA Code: {dma_code} --> DMA: {dma_label}')

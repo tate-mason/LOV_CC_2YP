797fbad7c6bb2a1b525a38062ff7bdeffd754397
tate-mason <tatem8499@gmail.com>
2026-09-09 16:11:02 -0400

    optimize

--- Code/dat_expo/code_check.py
+++ Code/dat_expo/code_check.py
@@ -19,7 +19,7 @@ pd.set_option(
 #    print(market_df.columns.to_list())
 #
 
-df_panel = pd.read_parquet(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/output_markets/full_panel.parquet')
+df_panel = pl.scan_parquet(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/output_markets/full_panel.parquet').to_pandas()
 
 print(df_panel.shape)
 print(df_panel['household_code'].nunique())

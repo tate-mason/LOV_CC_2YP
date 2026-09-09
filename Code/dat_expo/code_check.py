07f482241166306624e4244b2f4fce48fdeecebb
tate-mason <tatem8499@gmail.com>
2026-09-09 16:05:10 -0400

    bloat

--- Code/dat_expo/code_check.py
+++ Code/dat_expo/code_check.py
@@ -5,19 +5,19 @@ pd.set_option(
     'display.max_columns', None
 )
 
-df = pd.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documetation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx', dtype=str)
-
-mapping_df = df.iloc[:,[1, 2, 3, 4,5]].drop_duplicates().reset_index(drop=True)
-
-print(mapping_df)
-
-dat   = ['panelists', 'trips', 'retailer', 'product_attr', 'product_desc', 'purchases']
-years = [2022, 2023, 2024]
-
-for d, y in product(dat, years):
-    market_df = pd.read_parquet(f"/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{d}_{y}.parquet")
-    print(market_df.columns.to_list())
-
+#df = pd.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documetation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx', dtype=str)
+#
+#mapping_df = df.iloc[:,[1, 2, 3, 4,5]].drop_duplicates().reset_index(drop=True)
+#
+#print(mapping_df)
+#
+#dat   = ['panelists', 'trips', 'retailer', 'product_attr', 'product_desc', 'purchases']
+#years = [2022, 2023, 2024]
+#
+#for d, y in product(dat, years):
+#    market_df = pd.read_parquet(f"/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{d}_{y}.parquet")
+#    print(market_df.columns.to_list())
+#
 
 df_panel = pd.read_parquet(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/output_markets/full_panel.parquet')
 

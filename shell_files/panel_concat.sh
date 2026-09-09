3174e9a2b7c891a05382cc476b5fc7d09e316b4e
tate-mason <tatem8499@gmail.com>
2026-09-09 15:03:46 -0400

    la

--- shell_files/panel_concat.sh
+++ shell_files/panel_concat.sh
@@ -14,4 +14,4 @@ ml Python/3.12.3-GCCcore-13.3.0 # Load software module and run bowtie2 below
 
 pip install -qqq -r /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/shell_files/requirements.txt
 
-python /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/Code/dat_expo/code_check.py
+python /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/Code/dat_expo/panel_dat/panel_concat.py

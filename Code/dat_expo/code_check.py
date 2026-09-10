74dd3b73058da8e0c1eee5e3097396cedf3b7b50
tate-mason <tatem8499@gmail.com>
2026-09-10 08:46:13 -0400

    test

--- Code/dat_expo/code_check.py
+++ Code/dat_expo/code_check.py
@@ -2,26 +2,26 @@ from itertools import product
 import pandas as pd
 import polars as pl
 
-pd.set_option(
-    'display.max_rows', None,
-    'display.max_columns', None
-)
-
-df = pd.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documetation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx', dtype=str)
-
-mapping_df = df.iloc[:,[1, 2, 3, 4,5]].drop_duplicates().reset_index(drop=True)
-
-print(mapping_df)
-
-dat   = ['panelists', 'trips', 'retailer', 'product_attr', 'product_desc', 'purchases']
-years = [2022, 2023, 2024]
-
-for d, y in product(dat, years):
-    market_df = pd.read_parquet(f"/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{d}_{y}.parquet")
-    print(market_df.columns.to_list())
-
-
-#df_panel = pl.scan_parquet(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/output_markets/full_panel.parquet').to_pandas()
+#pd.set_option(
+#    'display.max_rows', None,
+#    'display.max_columns', None
+#)
 #
-#print(df_panel.shape)
-#print(df_panel['household_code'].nunique())
+#df = pd.read_excel('/scratch/dtm63837/Kilts_Panel/Reference_Documentation/2021-Onward_Documetation/FIPS-SMM-DMA-Conversion-Table-2021.xlsx', dtype=str)
+#
+#mapping_df = df.iloc[:,[1, 2, 3, 4,5]].drop_duplicates().reset_index(drop=True)
+#
+#print(mapping_df)
+#
+#dat   = ['panelists', 'trips', 'retailer', 'product_attr', 'product_desc', 'purchases']
+#years = [2022, 2023, 2024]
+#
+#for d, y in product(dat, years):
+#    market_df = pd.read_parquet(f"/scratch/dtm63837/Kilts_Panel/nielsen_extracts/HMS/{d}_{y}.parquet")
+#    print(market_df.columns.to_list())
+
+
+df_panel = pl.scan_parquet(f'/scratch/dtm63837/Kilts_Panel/nielsen_extracts/output_markets/full_panel.parquet').to_pandas()
+
+print(df_panel.shape)
+print(df_panel['household_code'].nunique())

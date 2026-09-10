c92db9660845936c622f6a5b3c52dbdcf6a2e2c2
tate-mason <tatem8499@gmail.com>
2026-09-10 17:43:44 -0400

    stupid typo

--- Code/full_draft_1231/data.py
+++ Code/full_draft_1231/data.py
@@ -207,8 +207,8 @@ Yogurt Only:
     - mean consecutive buys
 """
 
-agent_master['quantity'] = pd.to_numeric(df['quantity'], errors='coerce')
-agent_master['household_income'] = pd.to_numeric(df['household_income'], errors='coerce')
+agent_master['quantity'] = pd.to_numeric(agent_master['quantity'], errors='coerce')
+agent_master['household_income'] = pd.to_numeric(agent_master['household_income'], errors='coerce')
 
 agent_master['male_head_age'] = agent_master['male_head_age'].replace(0, np.nan)
 agent_master['head_age'] = agent_master['male_head_age'].fillna(agent_master['female_head_age'])

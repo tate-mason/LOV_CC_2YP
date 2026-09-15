f2250b8b595e79f20311b8d9134db249ad6595ad
tate-mason <tatem8499@gmail.com>
2026-09-15 16:52:42 -0400

    retail data conversion

--- Code/dat_expo/retail_dat/retail_convert.py
+++ Code/dat_expo/retail_dat/retail_convert.py
@@ -2,57 +2,52 @@ import polars as pl
 import gc
 import os
 
-outpath = '/scratch/dtm63837/Kilts_Panel/RMS/'
+years = [2022, 2023, 2024]
 
-years = [2017, 2018, 2019]
-markets = []
+base_raw_dir = "/scratch/dtm63837/Kilts_Panel/nielsen_extracts/RMS"
+base_out_dir = "/scratch/dtm63837/Kilts_Panel/nielsen_extracts/RMS"
 
-print("Loading RMS Data:")
-rms = ( 
-    pl.scan_csv('/scratch/dtm63837/Kilts_Panel/RMS/2014/Annual_Files/rms_versions_2014.tsv', separator = '\t', quote_char=None, encoding='utf8-lossy')
-)
-rms.sink_parquet(f'{outpath}rms.parquet')
-print("RMS Data Converted")
+os.makedirs(base_out_dir, exist_ok=True)
 
-print('Loading Store Data')
-stores = (
-    pl.scan_csv('/scratch/dtm63837/Kilts_Panel/RMS/2014/Annual_Files/stores_2014.tsv', separator = '\t', quote_char=None, encoding='utf8-lossy')
-)
-stores.sink_parquet(f'{outpath}stores.parquet')
-print('Store Data Converted')
+files_to_convert = {
+    'productattributes.tsv': 'attributes',
+    'productdesc.tsv': 'description',
+    'producthierarchy.tsv': 'hierarchy',
+    'stores.tsv':           'stores'
+}
 
-print('Loading Movement Data')
-raw_movement3603 =(
-    pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/RMS/2014/Movement_Files/2510_2014/3603_2014.tsv', separator='\t', quote_char=None, encoding='utf8-lossy')
-    #.sink_parquet('../../movement_3603.parquet')
-)
+for y in years:
+    print(f'=== Converting files for {y} ===')
+    for tsv_name, out_name in files_to_convert.items():
+        tsv_path    = f"{base_raw_dir}/{y}/Annual_Files/{tsv_name}"
+        parquet_out = f"{base_out_dir}/{out_name}_{y}.parquet"
 
-raw_movement3612 = (
-    pl.scan_csv(f'/scratch/dtm63837/Kilts_Panel/RMS/2014/Movement_Files/2510_2014/3612_2014.tsv', separator='\t', quote_char=None, encoding='utf8-lossy')
-    #.sink_parquet('../../movement_3612.parquet')
-)
+        if os.path.exists(tsv_path):
+            (pl.scan_csv(
+                tsv_path,
+                separator = "\t",
+                quote_char = None,
+                infer_schema_length = 0
+            )
+            .rename(str.lower)
+            .sink_parquet(parquet_out)
+            )
+            print(f"Converted {tsv_name} -> {out_name}_{y}.parquet")
+        else:
+            print(f"WARNING: File not found {tsv_path}")
+    print(f'===Converting Movement Files===')
+    tsv_path       = f"{base_raw_dir}/GROCERY/YOGURT_{y}.tsv"
+    parquet_out    = f"{base_out_dir}/movement_{y}.parquet"
 
-raw_move = (
-    pl.concat([raw_movement3603, raw_movement3612])
-)
-raw_move.sink_parquet(f'{outpath}movement.parquet')
+    (pl.scan_csv(
+        tsv_path,
+        separator  = "\t",
+        quote_char = None,
+        infer_schema_length = 0
+    )
+    .rename(str.lower)
+    .sink_parquet(parquet_out)
+    )
 
-print('Movement Data Converted')
 
-print("loading products")
-products = (
-    pl.scan_csv('/scratch/dtm63837/Kilts_Panel/RMS/Master_Files_2006-2020/Latest/products.tsv', separator='\t', quote_char=None, encoding='utf8-lossy')
-    .select(['upc', 'upc_ver_uc', 'product_group_code', 'product_module_code', 'brand_code_uc', 'size1_code_uc', 'size1_units'])
-)
-products.sink_parquet(f'{outpath}products.parquet')
-print('Products converted')
 
-#print(products.columns)
-#print(rms.columns)
-#print(stores.columns)
-#print(raw_movement3603.columns)
-#print(raw_movement3612.columns)
-
-print('-'*60)
-print('All Retail Files Coverted from .csv to .parquet -- Move to Merge')
-print('-'*60)

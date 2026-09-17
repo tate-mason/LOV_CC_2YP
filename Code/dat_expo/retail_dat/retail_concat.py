85023a26bfc9f046a29d2d32bc852b47e97c86ce
tate-mason <tatem8499@gmail.com>
2026-09-17 14:20:54 -0400

    la

--- Code/dat_expo/retail_dat/retail_concat.py
+++ Code/dat_expo/retail_dat/retail_concat.py
@@ -3,4 +3,27 @@ import polars as pl
 
 output_dir = "/scratch/dtm63837/Kilts_Panel/nielsen_extracts/output_markets"
 
+market_codes = [
+    "Atlanta", "Chicago", "Houston", "Denver", 
+    "Phoenix", "Philadelphia", "San_Diego", "Des_Moines"
+]
 
+market_dfs = []
+for m in market_codes:
+    file_path = os.path.join(output_dir, f'master_retail_{m}.parquet')
+
+    if os.path.exists(file_path):
+        lazy_df = (
+            pl.scan_parquet(file_path)
+            .rename(str.lower)
+            .with_columns(pl.lit(m).alias("market_name"))
+        )
+        market_dfs.append(lazy_df)
+
+    else:
+        print(f"Skipping missing file: {file_path}")
+
+if market_dfs:
+    combined_lazy = pl.concat(market_dfs, how='diagonal_relaxed')
+    combined_lazy.sink_parquet(os.path.join(output_dir, 'full_retail.parquet'))
+    print('--> Saved master retail parquet file (full_retail.parquet')

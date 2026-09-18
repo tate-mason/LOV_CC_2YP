40357b64b6db0d384b6a22599a77c864294924c2
tate-mason <tatem8499@gmail.com>
2026-09-18 08:24:01 -0400

    add git pull to shell file

--- shell_files/summ_desc.sh
+++ shell_files/summ_desc.sh
@@ -10,6 +10,8 @@
 #SBATCH --mail-user=dtm63837@uga.edu    # Where to send mail
 #SBATCH --mail-type=END,FAIL            # Mail events (BEGIN, END, FAIL, ALL)
 
+git pull
+
 ml Python/3.13.5-GCCcore-14.3.0 # Load software module and run bowtie2 below
 
 pip install -qqq -r /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/shell_files/requirements.txt

07cc5d458027f209a6fd8eceb1de7d5f0ac5fd9d
tate-mason <dtm63837@uga.edu>
2026-09-22 16:57:48 -0400

    output

--- shell_files/retail.sh
+++ shell_files/retail.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1			# Number of compute nodes for resources to be spread out over (increase only if using MPI enabled software)
 #SBATCH --ntasks=1			# 1 task (process) for below commands
 #SBATCH --cpus-per-task=1	 	# CPU core count per task, by default 1 CPU core per task
-#SBATCH --mem=300G			# Memory per node (4GB); by default using M as unit
+#SBATCH --mem=900G			# Memory per node (4GB); by default using M as unit
 #SBATCH --time=6:00:00              	# Time limit hrs:min:sec or days-hours:minutes:seconds
 #SBATCH --output=%x_%j.txt              # Standard output log, e.g., testBowtie2_12345.out
 #SBATCH --mail-user=dtm63837@uga.edu    # Where to send mail
@@ -18,6 +18,6 @@ set -e
 
 git pull
 
-python /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/Code/dat_expo/retail_dat/retail_convert.py
-python /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/Code/dat_expo/retail_dat/retail_merge.py
-#python /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/Code/dat_expo/retail_dat/retail_concat.py
+#python /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/Code/dat_expo/retail_dat/retail_convert.py
+#python /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/Code/dat_expo/retail_dat/retail_merge.py
+python /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/Code/dat_expo/retail_dat/retail_concat.py
--- shell_files/summ_desc.sh
+++ shell_files/summ_desc.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 #SBATCH --job-name=summCols # Job name (testBowtie2)
-#SBATCH --partition=highmem_p   # Partition name (batch, highmem_p, or gpu_p)
+#SBATCH --partition=hugemem_p   # Partition name (batch, highmem_p, or gpu_p)
 #SBATCH --nodes=1           # Number of compute nodes for resources to be spread out over (increase only if using MPI enabled software)
 #SBATCH --ntasks=1          # 1 task (process) for below commands
-#SBATCH --cpus-per-task=1   # CPU core count per task, by default 1 CPU core per task
-#SBATCH --mem=500G          # Memory per node (4GB); by default using M as unit
+#SBATCH --cpus-per-task=8   # CPU core count per task, by default 1 CPU core per task
+#SBATCH --mem=900G          # Memory per node (4GB); by default using M as unit
 #SBATCH --time=6:00:00      # Time limit hrs:min:sec or days-hours:minutes:seconds
 #SBATCH --output=%x_%j.txt  # Standard output log, e.g., testBowtie2_12345.out
 #SBATCH --mail-user=dtm63837@uga.edu    # Where to send mail
 #SBATCH --mail-type=END,FAIL            # Mail events (BEGIN, END, FAIL, ALL)
 
-git pull
-
 ml Python/3.13.5-GCCcore-14.3.0 # Load software module and run bowtie2 below
 
 pip install -qqq -r /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/shell_files/requirements.txt
 
-python /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/Code/full_draft_1231/data.py
+set -e
+
+git pull
+
+python /scratch/dtm63837/Kilts_Panel/LOV_CC_2YP/Code/full_draft_1231/data_merge.py
